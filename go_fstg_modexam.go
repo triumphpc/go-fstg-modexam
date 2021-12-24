ba4cb39065bdf9c680b3a57268ad27df62e3f986
Sergey Vrulin <triumph.job@gmail.com>
2021-12-24 19:48:35 +0300

    Change module path

--- go_fstg_modexam.go
+++ go_fstg_modexam.go
@@ -1,8 +1,14 @@
 package go_fstg_modexam
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 // Hi example for output
-func Hi(name string) string {
-	return fmt.Sprintf("Hello, %s! You are greate man!!!New changes", name)
+func Hi(name, info string) (string, error) {
+	if info == "" {
+		return "", errors.New("unknown information")
+	}
+	return fmt.Sprintf("Hello, %s!!!, \nInformation for you:%s", name, info), nil
 }
